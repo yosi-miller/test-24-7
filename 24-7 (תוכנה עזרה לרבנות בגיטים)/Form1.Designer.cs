fa9d75da411f7645663d9617dbda7eaa75559062
Yossi Miller <y0504199881@gmail.com>
2024-07-24 12:19:39 +0300

    Add project files.

---
+++ 24-7 (תוכנה עזרה לרבנות בגיטים)/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace _24_7__תוכנה_עזרה_לרבנות_בגיטים_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbmDayOfWeak = new System.Windows.Forms.ComboBox();
+            this.cbmDayOfMonth = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbmMonth = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbmYear = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1724, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "היום בשבוע";
+            // 
+            // cbmDayOfWeak
+            // 
+            this.cbmDayOfWeak.FormattingEnabled = true;
+            this.cbmDayOfWeak.Items.AddRange(new object[] {
+            "ראשון",
+            "שני",
+            "שלישי",
+            "רביעי",
+            "חמישי",
+            "ששי"});
+            this.cbmDayOfWeak.Location = new System.Drawing.Point(1637, 277);
+            this.cbmDayOfWeak.Name = "cbmDayOfWeak";
+            this.cbmDayOfWeak.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cbmDayOfWeak.Size = new System.Drawing.Size(258, 39);
+            this.cbmDayOfWeak.TabIndex = 2;
+            // 
+            // cbmDayOfMonth
+            // 
+            this.cbmDayOfMonth.FormattingEnabled = true;
+            this.cbmDayOfMonth.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30"});
+            this.cbmDayOfMonth.Location = new System.Drawing.Point(1171, 277);
+            this.cbmDayOfMonth.Name = "cbmDayOfMonth";
+            this.cbmDayOfMonth.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cbmDayOfMonth.Size = new System.Drawing.Size(258, 39);
+            this.cbmDayOfMonth.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1258, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(373, 80);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "היום בחודש";
+            // 
+            // cbmMonth
+            // 
+            this.cbmMonth.FormattingEnabled = true;
+            this.cbmMonth.Items.AddRange(new object[] {
+            "תשרי",
+            "מרחשון",
+            "כסלו",
+            "טבת",
+            "שבט",
+            "אדר",
+            "אדר א",
+            "אדר ב",
+            "ניסן",
+            "אייר",
+            "סיון",
+            "תמוז",
+            "אב",
+            "אלול"});
+            this.cbmMonth.Location = new System.Drawing.Point(726, 277);
+            this.cbmMonth.Name = "cbmMonth";
+            this.cbmMonth.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cbmMonth.Size = new System.Drawing.Size(258, 39);
+            this.cbmMonth.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(813, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 32);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "חודש";
+            // 
+            // cbmYear
+            // 
+            this.cbmYear.FormattingEnabled = true;
+            this.cbmYear.Items.AddRange(new object[] {
+            "תשפ\"ד",
+            "תשפ\"ה",
+            "תשפ\"ו",
+            "תשפ\"ז",
+            "תשפ\"ח",
+            "תשפ\"ט",
+            "תש\"צ",
+            "תשצ\"א",
+            "תשצ\"ב",
+            "תשצ\"ג"});
+            this.cbmYear.Location = new System.Drawing.Point(333, 277);
+            this.cbmYear.Name = "cbmYear";
+            this.cbmYear.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cbmYear.Size = new System.Drawing.Size(258, 39);
+            this.cbmYear.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(420, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 32);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "שנה";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(333, 527);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(228, 103);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "כתיבה";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(533, 861);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblResult.Size = new System.Drawing.Size(89, 32);
+            this.lblResult.TabIndex = 10;
+            this.lblResult.Text = "תוצאה";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2130, 1101);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cbmYear);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbmMonth);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbmDayOfMonth);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbmDayOfWeak);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbmDayOfWeak;
+        private System.Windows.Forms.ComboBox cbmDayOfMonth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbmMonth;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbmYear;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ 24-7 (תוכנה עזרה לרבנות בגיטים)/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace _24_7__תוכנה_עזרה_לרבנות_בגיטים_
+{
+    public partial class Form1 : Form
+    {
+        // מה שנמצא על רמי הדייג על החכה
+        XmlDocument xmlDocument;
+        // נתיב לקובץ המקומי
+        string pathName = Directory.GetCurrentDirectory() + "\\quriesData.xml";
+        public Form1()
+        {
+            InitializeComponent();
+            xmlDocument = new XmlDocument();
+
+            // check if the file is created
+            if (File.Exists(pathName))
+            {
+                // if the file is created load the xml file
+                try
+                {
+                    // open the xml file
+                    xmlDocument.Load(pathName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading XML file " + ex.Message);
+                }
+            }
+            else
+            {
+                XmlNode root = xmlDocument.CreateElement("Quries");
+
+                xmlDocument.AppendChild(root);
+                xmlDocument.Save(pathName);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string result;
+            string dayWeak = dayOfWeak(cbmDayOfWeak.Text);
+            string dayMonth = dayOfMonth(cbmDayOfMonth.Text, cbmMonth.Text);
+            string month = month1(cbmMonth.Text);
+            string year = year1(cbmYear.Text);
+
+
+            if (cbmDayOfMonth.Text == "30")
+            {
+                result = dayWeak + dayMonth + year;
+            }
+            else 
+            { 
+                result = dayWeak + dayMonth + month + year; 
+            }
+
+            XmlNode query = xmlDocument.CreateElement("Query");
+
+            query.AppendChild(xmlDocument.CreateElement("Day")).InnerText = cbmDayOfWeak.Text;
+            query.AppendChild(xmlDocument.CreateElement("DayMonth")).InnerText = cbmDayOfMonth.Text;
+            query.AppendChild(xmlDocument.CreateElement("Month")).InnerText = cbmMonth.Text;
+            query.AppendChild(xmlDocument.CreateElement("Year")).InnerText = cbmYear.Text;
+            query.AppendChild(xmlDocument.CreateElement("Result")).InnerText = dayWeak + dayMonth + month + year;
+            xmlDocument.FirstChild.AppendChild(query);
+            xmlDocument.Save(pathName);
+            MessageBox.Show("נוסף בהצלחה");
+            lblResult.Text = result;
+        }
+
+        private string dayOfWeak(string dayWeak)
+        {
+            string result = "";
+
+            switch (dayWeak)
+            {
+                case "ראשון":
+                    result = " באחד בשבת";
+                    break;
+                case "שני":
+                    result = " בשני בשבת";
+                    break;
+                case "שלישי":
+                    result = "בשלישי בשבת ";
+                    break;
+                case "רביעי":
+                    result = " ברביעי בשבת";
+                    break;
+                case "חמישי":
+                    result = " בחמישי בשבת";
+                    break;
+                case "ששי":
+                    result = " בששי בשבת";
+                    break;
+                default:
+                    result = "";
+                    break;
+            }
+            return result;
+        }
+
+        private string dayOfMonth(string dayMonth, string month)
+        {
+            string result = "";
+
+            switch (dayMonth)
+            {
+                case "1":
+                    result = " יום אחד לירח";
+                    break;
+                case "2":
+                    result = " שני ימים לירח";
+                    break;
+                case "3":
+                    result = " שלשה ימים לירח";
+                    break;
+                case "4":
+                    result = " ארבעה ימים לירח";
+                    break;
+                case "5":
+                    result = " חמשה ימים לירח";
+                    break;
+                case "6":
+                    result = " ששה ימים לירח";
+                    break;
+                case "7":
+                    result = " שבעה ימים לירח";
+                    break;
+                case "8":
+                    result = " שמנה ימים לירח";
+                    break;
+                case "9":
+                    result = " תשעה ימים לירח";
+                    break;
+                case "10":
+                    result = "עשרה ימים לירח ";
+                    break;
+                case "11":
+                    result = " אחד עשר יום לירח";
+                    break;
+                case "12":
+                    result = " שנים עשר יום לירח";
+                    break;
+                case "13":
+                    result = " שלשה עשר יום לירח";
+                    break;
+                case "14":
+                    result = " ארבעה עשר יום לירח";
+                    break;
+                case "15":
+                    result = " חמשה עשר יום לירח";
+                    break;
+                case "16":
+                    result = " ששה עשר יום לירח";
+                    break;
+                case "17":
+                    result = " שבעה עשר יום לירח";
+                    break;
+                case "18":
+                    result = "שמנה עשר יום לירח ";
+                    break;
+                case "19":
+                    result = " תשעה עשר יום לירח";
+                    break;
+                case "20":
+                    result = "עשרים יום לירח ";
+                    break;
+                case "21":
+                    result = "אחד ועשרים יום לירח";
+                    break;
+                case "22":
+                    result = " שנים ועשרים יום לירח";
+                    break;
+                case "23":
+                    result = " שלשה ועשרים יום לירח";
+                    break;
+                case "24":
+                    result = " ארבעה ועשרים יום לירח";
+                    break;
+                case "25":
+                    result = " חמשה ועשרים יום לירח";
+                    break;
+                case "26":
+                    result = " ששה ועשרים יום לירח";
+                    break;
+                case "27":
+                    result = " שבעה ועשרים יום לירח";
+                    break;
+                case "28":
+                    result = " שמנה ועשרים יום לירח";
+                    break;
+                case "29":
+                    result = " תשעה ועשרים יום לירח";
+                    break;
+                case "30":
+                    switch (month)
+                    {
+                        case "תשרי":
+                            result = "יום שלשים לחודש תשרי שהוא ראש חודש מרחשון ";
+                            break;
+                        case "מרחשון":
+                            result = " יום שלשים לחודש מרחשון שהוא ראש חודש כסלו";
+                            break;
+                        case "כסלו":
+                            result = " יום שלשים לחודש כסלו שהוא ראש חודש טבת";
+                            break;
+                        case "טבת":
+                            result = " יום שלשים לחודש טבת שהוא ראש חודש שבט";
+                            break;
+                        case "שבט":
+                            result = " יום שלשים לחודש טבת שהוא ראש חודש אדר";
+                            break;
+                        case "אדר":
+                            result = " יום שלשים לחודש אדר שהוא ראש חודש ניסן";
+                            break;
+                        case "אדר א":
+                            result = " יום שלשים לחודש אדר הראשון שהוא ראש חודש אדר השני";
+                            break;
+                        case "אדר ב":
+                            result = " יום שלשים לחודש אדר השני שהוא ראש חודש ניסן";
+                            break;
+                        case "ניסן":
+                            result = " יום שלשים לחודש ניסן שהוא ראש חודש אייר";
+                            break;
+                        case "אייר":
+                            result = "יום שלשים לחודש אייר שהוא ראש חודש סיון ";
+                            break;
+                        case "סיון":
+                            result = " יום שלשים לחודש סיון שהוא ראש חודש תמוז";
+                            break;
+                        case "תמוז":
+                            result = " יום שלשים לחודש תמוז שהוא ראש חודש אב";
+                            break;
+                        case "אב":
+                            result = " יום שלשים לחודש אב שהוא ראש חודש אלול";
+                            break;
+                        case "אלול":
+                            result = " יום שלשים לחודש אלול שהוא ראש חודש תשרי";
+                            break;
+                        default:
+                            result = "";
+                            break;
+                    }
+                    break;
+                default:
+                    result = "";
+                    break;
+
+            }
+            return result;
+        }
+
+        private string month1(string month)
+        {
+            string result = "";
+
+            switch (month)
+            {
+                case "תשרי":
+                    result = "תשרי ";
+                    break;
+                case "מרחשון":
+                    result = " מרחשון";
+                    break;
+                case "כסלו":
+                    result = " כסלו";
+                    break;
+                case "טבת":
+                    result = " טבת";
+                    break;
+                case "שבט":
+                    result = " שבט";
+                    break;
+                case "אדר":
+                    result = " אדר";
+                    break;
+                case "אדר א":
+                    result = " אדר הראשון";
+                    break;
+                case "אדר ב":
+                    result = " אדר השני";
+                    break;
+                case "ניסן":
+                    result = "ניסן ";
+                    break;
+                case "אייר":
+                    result = " אייר";
+                    break;
+                case "סיון":
+                    result = " סיון";
+                    break;
+                case "תמוז":
+                    result = " תמוז";
+                    break;
+                case "אב":
+                    result = " אב";
+                    break;
+                case "אלול":
+                    result = "אלול";
+                    break;
+                default:
+                    result = "";
+                    break;
+            }
+            return result;
+        }
+        private string year1(string year)
+        {
+            string result = "";
+            switch (year)
+            {
+                case "תשפ\"ד":
+                    result = " בשנת חמשת אלפים ושבע מאות ושמונים וארבע לבריאת העולם";
+                    break;
+                case "תשפ\"ה":
+                    result = " בשנת חמשת אלפים ושבע מאות ושמונים וחמש לבריאת העולם";
+                    break;
+                case "תשפ\"ו":
+                    result = " בשנת חמשת אלפים ושבע מאות ושמונים ושש לבריאת העולם";
+                    break;
+                case "תשפ\"ז":
+                    result = " בשנת חמשת אלפים ושבע מאות ושמונים ושבע לבריאת העולם";
+                    break;
+                case "תשפ\"ח":
+                    result = " בשנת חמשת אלפים ושבע מאות ושמונים ושמנה לבריאת העולם";
+                    break;
+                case "תשפ\"ט":
+                    result = " בשנת חמשת אלפים ושבע מאות ושמונים ותשע לבריאת העולם";
+                    break;
+                case "תש\"צ":
+                    result = "בשנת חמשת אלפים ושבע מאות ותשעים לבריאת העולם ";
+                    break;
+                case "תשצ\"א":
+                    result = "בשנת חמשת אלפים ושבע מאות ותשעים ואחת לבריאת העולם ";
+                    break;
+                case "תשצ\"ב":
+                    result = " בשנת חמשת אלפים ושבע מאות ותשעים ושנים לבריאת העולם";
+                    break;
+                case "תשצ\"ג":
+                    result = " בשנת חמשת אלפים ושבע מאות ותשעים ושלש לבריאת העולם";
+                    break;
+                default:
+                    result = "";
+                    break;
+            }
+            return result;
+        }
+    }
+}
